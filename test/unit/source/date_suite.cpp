f193d30c83be21582dd1bdc8c7b48a1a5c23f0fc
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2014-08-08 08:10:21 +0100

    completed date unit test.

--- test/unit/source/date_suite.cpp
+++ test/unit/source/date_suite.cpp
@@ -21,9 +21,23 @@ using framework::Date;
 
 //External Namespaces
 
-TEST( Date, format )
+TEST( Date, zero_format )
+{
+    string actual = Date::format( system_clock::from_time_t( 0 ) );
+    
+    EXPECT_EQ( "Thu Jan  1 00:00:00 1970", actual );
+}
+
+TEST( Date, positive_format )
 {
     string actual = Date::format( system_clock::from_time_t( 1402138994 ) );
     
-    EXPECT_EQ( "Sat Jun  7 07:03:14 2014", actual );
+    EXPECT_EQ( "Sat Jun  7 11:03:14 2014", actual );
+}
+
+TEST( Date, negative_timestamp )
+{
+    string actual = Date::format( system_clock::from_time_t( -100 ) );
+    
+    EXPECT_EQ( "Wed Dec 31 23:58:20 1969", actual );
 }
